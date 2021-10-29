7dd3e5c44dac3bd7677ba8e26d4eac96eebfd39b
choiappear <sodlfdms56@gmail.com>
2021-10-29 22:36:40 +0900

    제발...

--- marketKurlyty/src/main/java/first/market/kurlyty/vo/CategoryMainVO.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/vo/CategoryMainVO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CategoryMainVO {
+	
 	private String category_main_serial;
 	private String category_main_name;
 	private String category_main_icon_black;
--- marketKurlyty/src/main/java/first/market/kurlyty/vo/CategorySubVO.java
+++ marketKurlyty/src/main/java/first/market/kurlyty/vo/CategorySubVO.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class CategorySubVO {
+	
 	private String category_sub_serial;
 	private String category_sub_name;
 	private String category_sub_first_no;
